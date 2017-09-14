ae62b8672cd9c625734e99ff20dc9155bc9e7116
Armando Ruiz <armando.ruiz@industrie.co>
2017-09-14 10:54:47 +1000

    Configured custom routes for pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,16 @@
 Rails.application.routes.draw do
-
+  
+  #        get 'pages/home'
+  #        get 'pages/about'
+  #        get 'pages/contact'
+  
+  # The routes above are for statics pages, and we do not want to access to those pages => http://localhost:3000/pages/home instead we want to access http://localhost:3000/about and also we want home to be the root one, so when you type: http://localhost:3000 you go to pages/home. WE change the routes to:
+  
+  root to: 'pages#home'  # we now point http://localhost:3000/pages/home to ==> http://localhost:3000
+  
+  get 'about', to: 'pages#about' # we now point http://localhost:3000/pages/home to ==> http://localhost:3000/about
+  get 'contact', to: 'pages#contact' # we now point http://localhost:3000/pages/home to ==> http://localhost:3000/about
+  
+  
   resources :blogs
-
-  get 'pages/home'
-
-  get 'pages/about'
-
-  get 'pages/contact'
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
