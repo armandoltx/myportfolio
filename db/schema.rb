b2e5e286cbd31f2e8ce0b6784ff4b999a09b1420
armandoltx <armando@armandoltx.com>
2017-09-28 18:47:20 +1000

    created skills model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170926090156) do
+ActiveRecord::Schema.define(version: 20170928084412) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,13 @@ ActiveRecord::Schema.define(version: 20170926090156) do
     t.text "body"
     t.text "main_image"
     t.text "thumb_image"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "title"
+    t.string "percent_utilized"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
