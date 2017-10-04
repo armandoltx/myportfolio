f04e18fe88eefcf6b19711df464aab4ec3a834e9
armandoltx <armando@armandoltx.com>
2017-10-04 20:55:19 +1100

    Implemented callback set_default

--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,12 +1,20 @@
 class Portfolio < ApplicationRecord
   validates_presence_of :title, :body, :main_image, :thumb_image
-  
+
   # scopes
   # 1st method
-   def.self.angular
+   def self.angular
      where(subtitle: 'Angular')
    end
    # 2nd method
    scope :ruby_on_rails_portfolio_items, ->{ where(subtitle: "Ruby on Rails") }
-     
+
+   #callbacks
+   after_initialize :set_defaults
+
+   def set_defaults
+    self.main_image ||= "http://via.placeholder.com/400x400"
+    self.thumb_image ||= "http://via.placeholder.com/350x200"
+   end
+
 end
