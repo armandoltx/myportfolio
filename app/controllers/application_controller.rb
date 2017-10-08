3ed0ec0f7801f5d6c2abf8aca30352a880d13b50
armandoltx <armando@armandoltx.com>
2017-10-08 18:10:30 +1100

    Added controller concern for devise params

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,4 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-
-  before_action :configure_permitted_parameters, if: :devise_controller? # by using before_filter we say run this before everything else in control\llers because we are in app controller
-
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])           # only works in rails 5
-    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
-  end
+  include DeviseWhitelist
 end
