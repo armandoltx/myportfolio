f5e417478f1ba88e47f39108808db3bf9cb8e66c
armandoltx <armando@armandoltx.com>
2017-10-13 19:51:19 +1100

    fixing bugs

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,11 @@
-git diff class ApplicationController < ActionController::Base
+class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   include DeviseWhitelist
   include CurrentUserConcern
   include DefaultPageContentConcern
-  
+
   before_action :set_copyright
-  
+
   def set_copyright
     @copyright = LtxViewTool::Renderer.copyright('ArmandoLtx', 'All Rights Reserved')
   end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
     def login_helper
-        if current_user.is_a?(User) %> <!-- Doing it in order to remove the Logout button when we are logout. Since we added the current_user method we neede to fix it, we just check if current_user belongs to User clas or to OpenStruct class, if it belongs to User then do not show the button  -->
+        if current_user.is_a?(User)  # Doing it in order to remove the Logout button when we are logout. Since we added the current_user method we neede to fix it, we just check if current_user belongs to User clas or to OpenStruct class, if it belongs to User then do not show the button  
             link_to "Logout", destroy_user_session_path, method: :delete
         else
             (link_to "Register", new_user_registration_path) + "<br>.html_safe" +
