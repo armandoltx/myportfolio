750e2f3ef95fcf2887cac8aa0f672dfa547f929d
armandoltx <armando@armandoltx.com>
2017-10-15 19:00:48 +1100

    Integrated new gem we created into rails app for copyright

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,18 +3,4 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include CurrentUserConcern
   include DefaultPageContentConcern
-
-  before_action :set_copyright
-
-  def set_copyright
-    @copyright = LtxViewTool::Renderer.copyright('ArmandoLtx', 'All Rights Reserved')
-  end
-end
-
-module LtxViewTool
-  class Renderer
-    def self.copyright(name, msg)
-      "&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
-    end
-  end
 end
