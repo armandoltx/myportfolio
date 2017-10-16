0823d697ad7b3b8d04b1e08e2b9af51f5d06d148
Armando Ruiz <armando.ruiz@industrie.co>
2017-10-17 09:49:59 +1100

    Updated master layout styles along with helper methods

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,10 +1,10 @@
 module ApplicationHelper
-    def login_helper
-        if current_user.is_a?(GuestUser)  # Doing it in order to remove the Logout button when we are logout. Since we added the current_user method we neede to fix it, we just check if current_user belongs to GuestUser class or if it belongs to GuestUser then do not show the button
-          (link_to "Register", new_user_registration_path) + "<br>".html_safe +
-          (link_to "Login" , new_user_session_path)
+    def login_helper(style)
+        if current_user.is_a?(GuestUser)  # Doing it in order to remove the Logout button when we are logout. Since we added the current_user method we needed to fix it, we just check if current_user belongs to GuestUser class or if it belongs to GuestUser then do not show the button
+          (link_to "Register", new_user_registration_path, class: style) + " ".html_safe +
+          (link_to "Login" , new_user_session_path, class: style)
         else
-          link_to "Logout", destroy_user_session_path, method: :delete
+          link_to "Logout", destroy_user_session_path, method: :delete, class: style
         end
     end
 
