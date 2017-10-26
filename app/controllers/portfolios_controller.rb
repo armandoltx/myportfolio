0502b1ace31318c9e44c76e9fec77782a7dcbf60
Armando Ruiz <armando.ruiz@industrie.co>
2017-10-26 16:47:55 +1100

    Implemented authorization rules for portfolio controller and drag and drop feature

--- app/controllers/portfolios_controller.rb
+++ app/controllers/portfolios_controller.rb
@@ -1,7 +1,7 @@
 class PortfoliosController < ApplicationController
   before_action :set_portfolio_item, only: [:show, :edit, :update, :destroy]
   layout 'portfolio'
-  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit]}, site_admin: :all # petergate gem for  authorization check the user model to see what we have created
+  access all: [:show, :index, :angular], user: {except: [:destroy, :new, :create, :update, :edit, :sort]}, site_admin: :all # petergate gem for  authorization check the user model to see what we have created
 
   def index
     @portfolio_items ||= Portfolio.by_position
