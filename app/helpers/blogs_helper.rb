612bef8348c19ab1568118ab445a472c81765f35
Armando Ruiz <armando.ruiz@industrie.co>
2017-11-07 16:41:12 +1100

    Completed comment feature with action cable

--- app/helpers/blogs_helper.rb
+++ app/helpers/blogs_helper.rb
@@ -1,2 +1,5 @@
 module BlogsHelper
+  def gravatar_helper(user) #this is ti use gravatar, the user need to crate one in gravatar
+    image_tag "https://www.gravatar.com/avatar/#{Digest::MD5.hexdigest(user.email)}", width: 60
+  end
 end
