96c2c15004a22ae07a82a1b1dd1a0a67c1e2d1aa
Armando Ruiz <armando.ruiz@industrie.co>
2017-12-05 10:12:11 +1100

    Adding comments

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,7 +5,7 @@ class ApplicationController < ActionController::Base
   respond_to :html
 
   protect_from_forgery with: :exception
-  layout proc { false if request.xhr? }
+  layout proc { false if request.xhr? } #do not run the layout if there is an AJAX request(request.xhr?)
   include DeviseWhitelist
   include CurrentUserConcern
   include DefaultPageContentConcern
